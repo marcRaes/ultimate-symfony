3fb1fec1ec495c6fc974f19a7720ee257275a506
marcRAES <contact@marcraes.fr>
2022-03-25 12:41:06 +0100

    Découverte de fonctionnalités avancées du composant Form

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -4,14 +4,19 @@ namespace App\Form;
 
 use App\Entity\Product;
 use App\Entity\Category;
+use App\Form\DataTransformer\CentimesTransformer;
+use App\Form\Type\PriceType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 
 class ProductType extends AbstractType
 {
@@ -34,7 +39,8 @@ class ProductType extends AbstractType
                 'label' => 'Prix du produit',
                 'attr' => [
                     'placeholder' => 'Tapez le prix du produit en €'
-                ]
+                ],
+                'divisor' => 100
             ])
             ->add('mainPicture', UrlType::class, [
                 'label' => 'Image du produit',
@@ -50,6 +56,37 @@ class ProductType extends AbstractType
                     return strtoupper($category->getName());
                 }
             ]);
+
+        // $builder->get('price')->addModelTransformer(new CentimesTransformer);
+
+        // $builder->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) {
+        // /** @var Product */
+        //     $product = $event->getData();
+
+        //     if ($product->getPrice() !== null) {
+        //         $product->setPrice($product->getPrice() * 100);
+        //     }
+        // });
+
+        // $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+        // /** @var Product */
+        //     $product = $event->getData();
+
+        //     if ($product->getPrice() !== null) {
+        //         $product->setPrice($product->getPrice() / 100);
+        //     }
+
+        // if ($product->getId() === null) {
+        //         $form->add('category', EntityType::class, [
+        //             'label' => 'Categorie',
+        //             'placeholder' => '-- Choisir une catégorie --',
+        //             'class' => Category::class,
+        //             'choice_label' => function (Category $category) {
+        //                 return strtoupper($category->getName());
+        //             }
+        //         ]);
+        //     }
+        // });
     }
 
     public function configureOptions(OptionsResolver $resolver): void
